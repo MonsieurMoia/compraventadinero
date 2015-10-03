2eecfc75bbb79891b7c41129f51f5547eefca435
MonsieurMoia <moiacr@gmail.com>
2015-10-03 00:08:55 -0600

    express

--- index.js
+++ index.js
@@ -2,13 +2,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+//Express
 var app = express();
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
 
+//Routes
 app.get('/', function(req,res){
   res.send('Hello Im working');
 });
 
+// Start Server
 app.listen( process.env.PORT || 3000 );
 console.log("Running");
