9e24fee98bd8ac3c1f5453a12950562512d078d0
MonsieurMoia <moiacr@gmail.com>
2015-10-03 00:20:33 -0600

    express status

--- index.js
+++ index.js
@@ -2,16 +2,30 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+
+app.set('port', (process.env.PORT || 3000));
+
+
 //Express
 var app = express();
-app.use(bodyParser.urlencoded({ extended:true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended:true }));
+// app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
 //Routes
-app.get('/', function(req,res){
-  res.send('Hello Im working');
+app.get('/', function(request,response){
+  // res.send('Hello Im working');
+  response.render('pages/index')
+});
+app.get('/cool', function(request, response) {
+  response.send("fuck yeah");
 });
 
 // Start Server
-app.listen( process.env.PORT || 3000 );
-console.log("Running");
+app.listen(app.get('port'), function(){
+  console.log('Node app is running on port', app.get('port'));
+});
