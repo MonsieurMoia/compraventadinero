33dbed8739650c8c800fb748d6505c819f13fe19
Beta <Beta@Clastres.local>
2015-10-26 21:11:22 -0600

    control de usuarios con stormpath

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ var routes = require('./routes/index');
 var cursos = require('./routes/cursos');
 var form = require('./routes/form');
 var formnewcursos = require('./routes/formnewcursos.js');
+var stormpath = require('express-stormpath'); // Manejo de usuarios
 
 // console.log(typeof(Users.newUser));
 // Users.newUser("jesus","kwjebnwnleckw");
 
 // var mongoUri = process.env.MONGOLAB_URI ||
 //   'mongodb://admin:admin@ds051740.mongolab.com:51740/compraventa';
-
 
 // Bootstrap express
 //Express
@@ -31,6 +31,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
+// Manejo especial de usuarios.
+app.use(stormpath.init(app, {
+  apiKeyId: '3P9AQUE7UAZ9306NJ48L25YSS',
+  apiKeySecret: 'jhU2xtbYfQ7jF6E3YQbI3TC6PUrOak4iWUr7julv9xo',
+  application: 'https://api.stormpath.com/v1/applications/3mkxCG3iFhO0jjUaUNs0dR',
+  secretKey: 'uyuyuyestoessecreto',
+}));
+//
 
 
 // URLS management
